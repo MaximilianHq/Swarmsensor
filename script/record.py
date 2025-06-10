f6e60a76859b22cb598ff5b7fcfb6edbea7424ab
Maximilian Hallqvist <98323670+MaximilianHq@users.noreply.github.com>
2025-06-10 22:13:42 +0200

    push 2

--- script/record.py
+++ script/record.py
@@ -3,11 +3,10 @@ import pyaudio, numpy as np
 p = pyaudio.PyAudio()
 
 CHANNELS = 1 #number of audio channels (mono)
-CHUNK = 1024*2 #frames per buffer
+CHUNK = 256 #frames per buffer
 FORMAT = pyaudio.paInt16 #sample size (2 bytes)
-SAMPLE_RATE = 1024 #sample frequency
 
-def recordSample(rec_len:int):
+def recordSample(rec_len:int, SAMPLE_RATE:int):
     stream = p.open(
         channels = CHANNELS,
         format = FORMAT,
@@ -17,10 +16,9 @@ def recordSample(rec_len:int):
         frames_per_buffer = CHUNK
         )
 
-    print("recording")
-    print((SAMPLE_RATE/CHUNK)*rec_len)
+    print(f"recording sample {rec_len} s")
     chunks = list()
-    for i in range(0, int((SAMPLE_RATE/CHUNK)*rec_len)):
+    for i in range(0, (SAMPLE_RATE//CHUNK)*rec_len):
         data = stream.read(CHUNK)
         samples = np.frombuffer(data, dtype=np.int16)
         chunks.append(samples)
@@ -33,10 +31,3 @@ def recordSample(rec_len:int):
 
     signal = np.concatenate(chunks)
     return signal
-
-    #obj = wave.open(f"{path}{audio_sample_name}.wav", "wb")
-    #obj.setnchannels(CHANNELS)
-    #obj.setsampwidth(p.get_sample_size(FORMAT))
-    #obj.setframerate(SAMPLE_RATE)
-    #obj.writeframes(b"".join(frames))
-    #obj.close
